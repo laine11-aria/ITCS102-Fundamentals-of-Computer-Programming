6cb22c4d7613be46bdf32b62f10abadbaf4b88f5
laine11-aria <elainejavelosa169@gmail.com>
2026-08-26 18:55:21 +0800

    Add files via upload

---
+++ activity4.py
@@ -0,0 +1,12 @@
+#escape sequence -> to modify the behaviour of a linear formatted string
+# \n - new line 
+# \t - tab space
+# \r - carriage return/ deprecated (obsolete)
+# \b - back space
+# \\ - insert slash
+# \" - insert double quotation
+
+phrase = "Lewis Hamilton is a Formula 1 world\b\b\b\b\b champion \n\t\r\the has won 7 times in his ongoing 20-year career, \n\tHe has a total of \"106\" race wins and \\207 podium finishes"
+
+print(phrase)
+

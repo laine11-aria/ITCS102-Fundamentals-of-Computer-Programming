e40b449b62336ae5f3ac3c67a777deb9359a5ee5
laine11-aria <elainejavelosa169@gmail.com>
2026-08-26 18:56:41 +0800

    Add files via upload

---
+++ activity3.py
@@ -0,0 +1,38 @@
+#using variables
+
+#int
+age = 18
+weight = 39
+birthdate = 11
+siblings = 0
+
+#float
+temp = "23.5"
+speed = "30.4"
+pi = "3.14"
+balance = "8.56"
+
+#string 
+name = "peter_benjamin_parker"
+password = "gwen_bali_buto_102"
+username = "spidey"
+status = "yearner"
+
+#boolean
+yearner = True
+naka_Move_on_na = False
+kinalimutan = True
+idont_love_you = True
+
+print("My age is", age)
+print("My name is", name)
+print("My status is", status)
+print("yearner", yearner)
+print("naka_Move_on_na", naka_Move_on_na)
+print("kinalimutan", kinalimutan)
+print("idont_love_you", idont_love_you)
+
+
+
+
+

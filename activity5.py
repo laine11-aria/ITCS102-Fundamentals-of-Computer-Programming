545fd63dc28a621650c128f06234e1bb47c94ab7
laine11-aria <elainejavelosa169@gmail.com>
2026-08-26 20:18:36 +0800

    Add files via upload

---
+++ activity5.py
@@ -0,0 +1,25 @@
+#operators activity
+
+a = 11
+b = 7
+sum = a + b
+diff = a - b
+multiply = a * b
+divide = a / b
+modulus = a & b
+exponent = a ** b
+floor_div = a // b
+
+print("The sum of",a," +", b," is",sum)
+print("The difference of",a," -", b," is",diff)
+print("The product of",a," x", b," is",multiply)
+print("The quotient of",a," /", b," is",divide)
+print("The remainder of",a," %", b," %",modulus)
+print("The  of exponent",a," **", b," is",exponent)
+print("The floor division of",a," //", b," is",floor_div)
+
+
+
+
+
+
---
+++ code_challenge2.py
@@ -0,0 +1,45 @@
+#VARIABLES
+amount = 21539
+
+print("Money to deposit --->", amount)
+
+#computation here
+s = amount // 1000
+amount = amount % 1000
+p = amount // 500
+amount = amount % 500
+i = amount // 200
+amount = amount % 200
+d = amount // 100
+amount = amount % 100
+e = amount // 50
+amount = amount % 50
+r = amount // 20
+amount = amount % 20
+m = amount // 10
+amount = amount % 10
+a = amount // 5
+amount = amount % 5
+n = amount // 1
+amount = amount % 1
+
+
+print("the amount to be deposit has")
+
+
+print("1000 -", s)
+print("500 -", p)
+print("200 -", i)
+print("100 -", d)
+print("50 -", e)
+print("20 -", r)
+print("10 -", m)
+print("5 -", a)
+print("1 -", n)
+
+
+
+
+
+
+

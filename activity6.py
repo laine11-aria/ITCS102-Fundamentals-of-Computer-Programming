40bec20c297b5eb817c6bb132e649caf63c76c6b
laine11-aria <elainejavelosa169@gmail.com>
2026-09-02 11:15:27 +0800

    Add files via upload

---
+++ activity6.py
@@ -0,0 +1,8 @@
+#dynamic , static
+#dynamic - ever changing 
+#static - fixed
+                                                                                                                                                                                                                                                                        
+name_s = "Lourdes Maria Elaine A. Javelosa"
+name_d = input("What is your name ? --> ")
+print("Hello", name_s)
+print("Hello", name_d)

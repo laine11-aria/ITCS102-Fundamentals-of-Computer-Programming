3594f60450c850339916a60ff0088a19ebe44cb8
laine11-aria <elainejavelosa169@gmail.com>
2026-09-04 18:42:06 +0800

    Add files via upload

---
+++ activity7.py
@@ -0,0 +1,36 @@
+total = 10
+
+e = eval(input("enter any number --> ")) 
+total = total + e
+
+u = eval(input("enter any number --> ")) 
+total += u
+
+k = eval(input("enter any number --> ")) 
+total -= k
+
+a = eval(input("enter any number --> ")) 
+total *= a
+
+r = eval(input("enter any number --> ")) 
+total /= r
+
+y = eval(input("enter any number --> ")) 
+total *= y
+
+o = eval(input("enter any number --> ")) 
+total += o
+
+t = eval(input("enter any number --> ")) 
+total += t
+
+i = eval(input("enter any number --> ")) 
+total *= i
+
+c = eval(input("enter any number --> ")) 
+total /= c
+
+
+
+
+print("Current Total is --> ", total)
---
+++ activity8.py
@@ -0,0 +1,40 @@
+hobbies = "" #empty string
+
+e = input("What are your hobbies --> ") 
+hobbies += e + ", "
+
+u = input("What else --> ") 
+hobbies += u + ", "
+
+k = input("What else --> ") 
+hobbies += k + ", "
+
+a = input("What else --> ") 
+hobbies += a + ", "
+
+r = input("What else --> ") 
+hobbies += r + ", "
+
+y = input("What else --> ") 
+hobbies += y + ", "
+
+o = input("What else --> ") 
+hobbies += o + ", "
+
+t = input("What else --> ") 
+hobbies += t + ", "
+
+i = input("What else --> ") 
+hobbies += i + ", "
+
+c = input("What else --> ") 
+hobbies += c + ", "
+
+
+
+
+
+
+
+
+print("My hobbies are {", hobbies, "}")

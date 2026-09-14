44ef1ed96078ae228929fef09549e97eb096071e
laine11-aria <elainejavelosa169@gmail.com>
2026-09-14 21:09:42 +0800

    Add files via upload

---
+++ activity13.py
@@ -0,0 +1,36 @@
+age = int(input("Enter your age ---> "))
+is_employed = bool(input("Are you currently employed? "))
+credit_score = eval(input("Credit Score History ---> "))
+annual_income = eval(input("How much is your annual income---> ")) == True
+has_collateral = bool(input("Do you have any collateral? "))
+
+
+if age >= 21 and is_employed == True:
+    if credit_score >= 750:
+
+        base_interest_rate_tier1 = 5.0
+
+        if annual_income >= 100000:
+            interest_rate_tier1 = base_interest_rate_tier1 - 0.5
+        
+        print("Pwede na sa", interest_rate_tier1 , "% interest")
+
+    elif credit_score >= 600 and 750:
+        base_interest_rate_tier2 = 8.0
+
+        if has_collateral == True:
+            interest_rate_tier2 = base_interest_rate_tier2 - 1.0
+          
+        elif annual_income <= 40000 and has_collateral == False:
+            interest_rate_tier2 = base_interest_rate_tier2 + 1.5
+          
+        else:
+            interest_rate_tier2 = base_interest_rate_tier2
+
+        print("Pwede na sa", interest_rate_tier2, "% interest")
+
+    elif credit_score < 600:
+        print("Sayang! Credit score too low")
+
+else:
+    print("Baseline requirement FAILED!")
